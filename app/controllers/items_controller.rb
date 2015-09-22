0044f21fa861d4301e9ef4168d87bf7518c96254
KeiTamiya <webcreatorhouse@gmail.com>
2015-09-22 05:35:34 +0000

    create user and item view

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -16,6 +16,7 @@ class ItemsController < ApplicationController
     @large_image_url = @item.large_image
     @have_users = @item.have_users
     @want_users = @item.want_users
+    @user = User.find(params[:id])
   end
 
   private
